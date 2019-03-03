77e7b5f2bb53d70b3a6103d1e52992c667e1f940
Eduardo Ferreira <eduardo.zekl@gmail.com>
2019-03-03 13:45:35 -0300

    Repositório iniciado

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> AR DESENTUPIDORA </title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+
+            </div>
+            <div class="side-navigation">
+
+            </div>
+
+            <div class="content" id="loader">
+            </div>
+
+            <div class="footer">
+
+            </div>
+        </div>
+    </body>
+</html>
