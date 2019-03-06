807476eea2d07e103bca441eafffd02082a6f691
Eduardo Ferreira <eduardo.zekl@gmail.com>
2019-03-05 22:23:58 -0300

    Início da implementação da lógica

--- index.php
+++ index.php
@@ -18,13 +18,15 @@
 
                 <div class="slider">
                     <div class="sliderimg">
-                        <img src="resources/img/slider/1.jpg" alt="" />
+                        <img src="resources/img/slider/1.jpg" alt="" id="1" />
+                        <img src="resources/img/slider/2.jpg" alt="" id="3" />
+                        <img src="resources/img/slider/3.jpg" alt="" id="2" />
                     </div>
                     <div class="slidercontrollers">
                         <div class="circles">
-                            <div class="circle"></div>
-                            <div class="circle"></div>
-                            <div class="circle"></div>
+                            <div class="circle" id="slider-btn-1" class="trigger-slider tst"></div>
+                            <div class="circle" id="slider-btn-2" class="trigger-slider tst"></div>
+                            <div class="circle" id="slider-btn-3" class="trigger-slider tst"></div>
                         </div>
                         <div class="arrows">
                         </div>
@@ -40,3 +42,4 @@
     </body>
 </html>
 <script src="resources/js/jquery-3.3.1.min.js"></script>
+<script src="resources/js/slider.js"></script>
---
+++ resources/js/slider.js
@@ -0,0 +1,11 @@
+
+var current = 1;
+var next = 2;
+var prev = 3;
+
+
+$(document).ready(function(e){
+    $('.tst').click(function(){
+        alert('tst');
+    });
+});
