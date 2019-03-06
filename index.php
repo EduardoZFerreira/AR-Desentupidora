ec2a867c7a9cf0192d9468463b52a5cc0d2f28c1
Eduardo Ferreira <eduardo.zekl@gmail.com>
2019-03-05 21:51:46 -0300

    Criando base do slider

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <head>
         <link rel="shortcut icon" type="image/png" href="resources/img/favicon.png"/>
         <title> AR DESENTUPIDORA </title>
+        <link rel="stylesheet" type="text/css" href="resources/css/desktop/slider.css" />
+        <link rel="stylesheet" type="text/css" href="resources/css/desktop/main.css" />
     </head>
     <body>
         <div class="container">
@@ -13,6 +15,22 @@
 
             </div>
             <div class="content" id="loader">
+
+                <div class="slider">
+                    <div class="sliderimg">
+                        <img src="resources/img/slider/1.jpg" alt="" />
+                    </div>
+                    <div class="slidercontrollers">
+                        <div class="circles">
+                            <div class="circle"></div>
+                            <div class="circle"></div>
+                            <div class="circle"></div>
+                        </div>
+                        <div class="arrows">
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
             <div class="footer">
