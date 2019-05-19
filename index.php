b7d167764b2765563c2564841cf657127a14327b
Eduardo Ferreira <eduardo.zekl@gmail.com>
2019-05-19 19:52:14 -0300

    Alterações nas meta tags

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 <html>
     <head>
         <link rel="shortcut icon" type="image/png" href="resources/img/favicon.png"/>
-        <title> AR DESENTUPIDORA </title>
+        <title> AR DESENTUPIDORA E SERVIÇOS | JOINVILLE E REGIÃO </title>
         <link rel="stylesheet" type="text/css" href="resources/css/desktop/main.css" />
         <link rel="stylesheet" type="text/css" href="resources/css/mobile/main.css" />
         <link rel="stylesheet" type="text/css" href="resources/css/desktop/slider.css" />  
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" 
-		integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">		
-		
+		integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+		<meta name="description" content="Entre em contato com a desentupidora especializada e equipada para atender suas necessidades em Joinville e 
+		região, com os melhores preços. Trabalhamos também com estruturas metálicas e construção.">	
+		<meta name="robots" content="index, follow">
     </head>
     <body>
         <div class="container">
